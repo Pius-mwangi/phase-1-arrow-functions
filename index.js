2a7892664b8f0ccdf3a5a92cff2533dbcd1dba96
PIUS <relimoringa@gmail.com>
2023-06-15 13:38:37 +0300

    finished 5

---
+++ index.js
@@ -0,0 +1,17 @@
+let divide = (a, b = 100) => {
+    return a / b;
+};
+
+console.log(divide(2000, 100)); 
+
+let square = (multiply) => {
+    return multiply * multiply;
+};
+
+console.log(square(2)); 
+
+let add = (add1, add2) => {
+    return add1 + add2;
+};
+
+console.log(add(3, 4)); 
